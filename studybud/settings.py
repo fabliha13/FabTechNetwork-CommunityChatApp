19886b0338576d7e5030d070e340c64cba909f59
Fabliha Afaf Sarwar <fabliha.cs.doc@gmail.com>
2024-09-21 12:04:51 +0600

    Switched to PostgreSQL database

--- studybud/settings.py
+++ studybud/settings.py
@@ -88,11 +88,16 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
