3a674c5c5a2e1ae50a321c1c4d2c5a84fbdcae97
Fabliha Afaf Sarwar <fabliha.cs.doc@gmail.com>
2024-09-23 12:53:58 +0600

    Activity

--- studybud/settings.py
+++ studybud/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS=[
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+##FOR AWS
 # MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_URL = '/static/'
@@ -158,7 +160,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_ROOT =
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+##
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -168,4 +171,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 
+##For AWS
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')  # e.g., 'us-east-1'
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+##
 django_heroku.settings(locals())
